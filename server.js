94024cf8964f9eb2fcad90d507da9de477534943
krif014 <krif014@gmail.com>
2026-03-18 14:38:24 +0200

    Added the server.js

--- server.js
+++ server.js
@@ -1,5 +1,24 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const mongoose = require('mongoose');
-require('dotenv').config();
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import connectDB from "./config/db,js";
+import studentRoutes from "./routes/studentRoutes.js";
+import swaggerUi from "swagger-ui-express";
+import swaggerSpec from "./docs/swagger.js";
+
+dotenv.config();
+connectDB();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/students",studentRoutes);
+app.use("/api-docs",swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+     console.log(`Server running on port ${PORT}`);
+});
